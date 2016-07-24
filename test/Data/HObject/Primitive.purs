db5866ff766de6f1fcb3c5797e7a8bc4577145e1
Risto Stevcev <risto1@gmail.com>
2016-07-25 01:35:59 +0200

    Added helper functions and data types to work with primitive sum types

---
+++ test/Data/HObject/Primitive.purs
@@ -0,0 +1,33 @@
+module Example.Primitive where
+
+import Data.Tuple (Tuple)
+import Data.HObject.Primitive (class Primitive, (/^\))
+import Data.HObject.Primitive.Helpers (BoolOrInt, Prim)
+
+
+foo :: Array (Tuple String Prim)
+foo = [ "foo"  /^\ 2
+      , "bar"  /^\ 3.14
+      , "baz"  /^\ "qux"
+      , "norf" /^\ true
+      ]
+
+
+bar :: Array (Tuple String BoolOrInt)
+bar = [ "foo" /^\ true
+      , "bar" /^\ 3
+      ]
+
+
+data Baz = Qux Int | Norf String
+
+instance intToBaz :: Primitive Int Baz where
+  mkPrim = Qux 
+
+instance strToBaz :: Primitive String Baz where
+  mkPrim = Norf
+
+baz :: Array (Tuple String Baz)
+baz = [ "foo" /^\ 42
+      , "bar" /^\ "baz"
+      ]
